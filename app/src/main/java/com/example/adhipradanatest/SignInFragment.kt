81bb7b155688400a9798c9a3b21dd9d1322ccddc
ant <bearded.himenos@gmail.com>
2023-03-03 15:33:46 +0400

    simple navigation sign_in -> welcome_back -> profile

--- app/src/main/java/com/example/adhipradanatest/SignInFragment.kt
+++ app/src/main/java/com/example/adhipradanatest/SignInFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.adhipradanatest.databinding.FragmentSigninBinding
 
 class SignInFragment : Fragment() {
@@ -26,9 +27,16 @@ class SignInFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val navController = findNavController()
         super.onViewCreated(view, savedInstanceState)
         binding.frSignInLogInBtn.setOnClickListener {
-
+            navController.navigate(R.id.action_signInFr_to_welcomeBackFr)
+        }
+        binding.frSignInWithGoogleBtn.setOnClickListener {
+            navController.navigate(R.id.action_signInFr_to_profileFr)
+        }
+        binding.frSignInWithAppleBtn.setOnClickListener {
+            navController.navigate(R.id.action_signInFr_to_profileFr)
         }
     }
 
--- app/src/main/java/com/example/adhipradanatest/WelcomeBackFragment.kt
+++ app/src/main/java/com/example/adhipradanatest/WelcomeBackFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.adhipradanatest.databinding.FragmentWelcomeBackBinding
 
 class WelcomeBackFragment : Fragment() {
@@ -27,7 +28,9 @@ class WelcomeBackFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.frWelcomeBackLoginBtn.setOnClickListener { }
+        binding.frWelcomeBackLoginBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_welcomeBackFr_to_profileFr)
+        }
     }
 
     override fun onDestroyView() {
