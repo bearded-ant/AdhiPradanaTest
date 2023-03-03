3ad8c18702f5acfa807f115f40344b82789c2497
ant <bearded.himenos@gmail.com>
2023-03-03 14:51:45 +0400

    implement navigation. main nav_host

--- app/src/main/java/com/example/adhipradanatest/MainActivity.kt
+++ app/src/main/java/com/example/adhipradanatest/MainActivity.kt
@@ -2,14 +2,20 @@ package com.example.adhipradanatest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_profile)
+        setContentView(R.layout.activity_main)
 
-        val nav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        nav.itemIconTintList = null
+//        val navController = findNavController(R.id.main_fragmentContainerView)
+//        setupActionBarWithNavController(navController)
     }
+
+//    override fun onSupportNavigateUp(): Boolean {
+//        val navController = findNavController(R.id.main_fragmentContainerView)
+//        return navController.navigateUp() || super.onSupportNavigateUp()
+//    }
 }
